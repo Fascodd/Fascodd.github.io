97a34e42f66dbf8ba1e881b093dfb4f0766eda83
Joseph Cisneros <fascodd@gmail.com>
2018-12-05 13:43:05 -0800

    Update highlight.js
    Image text now matches the name of the image file

--- js/highlight.js
+++ js/highlight.js
@@ -2,8 +2,16 @@
 const galleryImage = document.getElementsByClassName("all");
 for (var i = 0; i < galleryImage.length; i++) {
   galleryImage[i].addEventListener("mouseover", shine);
-
   function shine() {
+    // sets iimage text to name of file
+    var img_text = this.children[0].children[0].src;
+    var remove_path = "http://127.0.0.1:3000/img/small_img/";
+    var remove_ext = ".png";
+    img_text = img_text.replace(remove_path, "");
+    img_text = img_text.replace(remove_ext, "");
+    this.children[1].innerHTML =
+      img_text.charAt(0).toUpperCase() + img_text.slice(1);
+    // fades in text
     this.style.transform = "translateY(5px)";
     this.style.transition = ".75s";
     this.style.zIndex = "1";
@@ -18,7 +26,6 @@ for (var i = 0; i < galleryImage.length; i++) {
   }
   // Mouseout event to return images to normal size and opacity
   galleryImage[i].addEventListener("mouseout", normal);
-
   function normal() {
     this.style.transform = "scale(1.0)";
     this.style.transition = ".75s";
