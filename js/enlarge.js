63c90bd54a2bfc773ac2920ce73211c398c59a34
Joseph Cisneros <fascodd@gmail.com>
2018-12-05 13:08:29 -0800

    added showcase
    Added showcase functionality

--- js/enlarge.js
+++ js/enlarge.js
@@ -1,9 +1,20 @@
 Object.values(galleryImage).forEach(function(image) {
   image.addEventListener("click", enlarge);
   function enlarge() {
-    var currentSrc = "/small_img";
+    // get src for larger version of image for showcase
     var currentImg = this.children[0].children[0].src;
+    var currentSrc = "/small_img";
     currentImg = currentImg.replace(currentSrc, "");
-    window.open(currentImg);
+    document.getElementsByClassName("showcase_img")[0].src = currentImg;
+    const showcase = document.getElementsByClassName("showcase")[0];
+    const showcase_img = document.getElementsByClassName("showcase_img")[0];
+    // display showcase image with background
+    showcase.style.display = "block";
+
+    //add event listener to close showcase
+    showcase.addEventListener("click", close);
+    function close() {
+      showcase.style.display = "none";
+    }
   }
 });
