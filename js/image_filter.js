f3d0a14bdf8d164bcdcafe243cf10c4c5fd63a75
Joseph Cisneros <fascodd@gmail.com>
2018-12-09 20:25:05 -0800

    filter animations
    changed filter animations to be based on position rather than just decreasing width

--- js/image_filter.js
+++ js/image_filter.js
@@ -5,16 +5,15 @@ const img_arr = document.getElementsByClassName("all");
 const gallery = document.getElementsByClassName("gallery")[0];
 const description = document.getElementsByClassName("description");
 var info_div = document.getElementsByClassName("info_div")[0];
-var first = [];
+var initial_pos = [];
 Object.values(img_arr).forEach(function(a) {
-  first.push(a.getBoundingClientRect());
+  initial_pos.push(a.getBoundingClientRect());
 });
 Object.values(butn).forEach(function(button) {
   button.addEventListener("click", filter);
   //Function, filter(), for EventListener
   function filter() {
-    var last = [];
-    console.log(first);
+    var final_pos = [];
     //Variables used in this filter
     var match_counter = 0;
     var non_selected = [];
@@ -41,24 +40,52 @@ Object.values(butn).forEach(function(button) {
       } else {
         non_selected.push(current_img);
       }
-      selected.forEach(function(image) {
-        var image_style = image.style;
+      selected.forEach(function(image_in) {
+        var image_style = image_in.style;
         image_style.transition = "0s";
         image_style.opacity = "1";
         image_style.width = "";
+        image_style.height = "";
       });
-      non_selected.forEach(function(image) {
-        var image_style = image.style;
+      non_selected.forEach(function(image_out) {
+        var image_style = image_out.style;
         image_style.transition = "0s";
         image_style.opacity = "0";
-        image_style.width = "0";
+        image_style.width = "1px";
+        image_style.height = "1px";
       });
     }
-    Object.values(img_arr).forEach(function(a) {
-      last.push(a.getBoundingClientRect());
+    Object.values(img_arr).forEach(function(image) {
+      final_pos.push(image.getBoundingClientRect());
     });
+    for (var i = 0; i < final_pos.length; i++) {
+      const deltaX = initial_pos[i].left - final_pos[i].left;
+      const deltaY = initial_pos[i].top - final_pos[i].top;
+      const deltaW = initial_pos[i].width / final_pos[i].width;
+      const deltaH = initial_pos[i].height / final_pos[i].height;
+      console.log(deltaX);
+      img_arr[i].animate(
+        [
+          {
+            transformOrigin: "top left",
+            transform: `
+    translate(${deltaX}px, ${deltaY}px)
+    scale(${deltaW}, ${deltaH})
+  `
+          },
+          {
+            transformOrigin: "top left ",
+            transform: "none"
+          }
+        ],
+        {
+          duration: 500,
+          easing: "ease-out",
+          fill: "both"
+        }
+      );
+    }
 
-    console.log(last);
-    first = last;
+    initial_pos = final_pos;
   }
 });
